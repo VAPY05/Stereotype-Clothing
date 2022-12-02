3c2f66bc56f1a17a1b17d8d77f39dc98528468ff
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-12-02 12:38:43 -0800

    creating app

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const hbs = require("express-handlebars")
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+
+const start = async () => {
+    try{
+        const db = await mongoose.connect("mongodb+srv://ProjectAngular:ZlFGuUtnZ5Sp9jDb@cluster0.f6sfuwx.mongodb.net/?retryWrites=true&w=majority");
+        console.log("DB Connected");
+    }
+    catch(error){
+        console.log("Error connecting to the database!")
+        return process.exit(1);
+    }
+
+    const app = express();
+
+    app.use(routes)
+
+    app.engine("hbs",hbs.engine({
+        extname: 'hbs'
+    }))
+
+    app.set("view engine","hbs")
+
+    app.listen("5000",()=>{console.log("The App Is Running On Port 5000...")})
+}
+
+start();
