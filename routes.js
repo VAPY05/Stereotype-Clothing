615edf7bd28a5cbf755f0e6c505528730b56596a
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-12-03 06:46:53 -0800

    adding the productController to the routes.js

--- routes.js
+++ routes.js
@@ -2,8 +2,10 @@ const routes = require("express").Router()
 
 const homeController = require('./controllers/homeController')
 const authController = require('./controllers/authController')
+const productsController = require('./controllers/productsController')
 
 routes.use(homeController)
 routes.use(authController)
+routes.use(productsController)
 
 module.exports = routes
