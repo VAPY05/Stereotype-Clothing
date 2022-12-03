dba099b9058f746b96696018221e3c9ddd55f5d4
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-12-03 06:34:54 -0800

    adding endpoints to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,26 @@
 const router = require("express").Router();
+const path = require("path")
 
 router.get("/profile",(req,res)=>{
-    res.render("home")
+    res.render("profile")
 })
 
 router.get("/profile/login",(req,res)=>{
     res.render("login")
 })
 
+router.post("/profile/login",(req,res)=>{
+    const {username, password} = req.body
+    res.send(`Username: ${username}, Password: ${password}`)
+})
+
 router.get("/profile/register",(req,res)=>{
     res.render("register")
+})
+
+router.post("/profile/register",(req,res)=>{
+    const {username, password, repeatPassword} = req.body
+    res.send(`Username: ${username}, Password: ${password}, Repeated Password: ${repeatPassword}`)
 })
 
 module.exports = router
