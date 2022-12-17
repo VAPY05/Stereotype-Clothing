9f9efed415c35cea293c49ea8863ce67649d33a3
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-12-17 08:11:47 -0800

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,30 @@
 const router = require("express").Router();
 
-router.get("/products",(req,res)=>{
-    res.render("catalog")
+const Clothing = require("../models/Cloth");
+const {create, find, findById} = require("../services/postService")
+
+router.get("/products",async(req,res)=>{
+    const data = await Clothing.find()
+    console.log(data)
+    res.render("catalog", {data})
 })
 
- router.get("/products/:id/details",(req,res)=>{
-    res.render("details")
- })
+router.get("/products/:id/details",async(req,res)=>{
+    const id = req.params.id;
+    const item =  await findById(id);
+    console.log(item)
+    res.render("details", {item})
+})
+
+router.get("/products/create",(req,res)=>{
+    res.render("create")
+})
+
+router.post("/products/create",async(req,res)=>{
+    const obj = req.body;
+    res.send(obj)
+    const data = await create(obj.name, obj.description, obj.img, obj.select, res.locals.user._id)
+    res.redirect("/products")
+})
 
 module.exports = router
