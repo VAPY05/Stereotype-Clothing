aacc771e13008719c841f10eb9b2c63d228f53e5
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2023-02-16 02:26:08 -0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,11 +1,9 @@
 const routes = require("express").Router()
 
 const homeController = require('./controllers/homeController')
-const authController = require('./controllers/authController')
 const productsController = require('./controllers/productsController')
 
 routes.use(homeController)
-routes.use(authController)
 routes.use(productsController)
 routes.get("*",(req,res)=>{res.render("404")})
 
